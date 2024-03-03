e1f4e5412e900047aa1f2d9b5932cc26b49115c1
zpub <lil.rhymes9@gmail.com>
2024-03-04 02:49:58 +0300

    Кликабельные карточки

--- pstuedu/src/components/Card.js
+++ pstuedu/src/components/Card.js
@@ -3,13 +3,14 @@ import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
 
 import "../css/card.css"
 
+
 export default function Card({title, href}) {
     return (
-            <a className="link" href={href}>
-                <div className="card">
-                    <InsertDriveFileIcon color="action" className="image" width={24} height={22}/>
+        <a href={href} className="card link">
+                <div className="card_div">
+                    <InsertDriveFileIcon color="primary" className="image" width={24} height={24}/>
                     <div className="title">{title}</div>
                 </div>
-            </a>
+        </a>
     )
 }
--- pstuedu/src/components/LessonPage.js
+++ pstuedu/src/components/LessonPage.js
@@ -2,17 +2,24 @@ import React from 'react'
 import {Paper} from "@mui/material";
 import Container from "@mui/material/Container";
 import Card from "./Card";
+import "../css/lessonpage.css";
 
 export default function LessonPage() {
     return (
         <div>
             <Container>
-                <h1>Название курса</h1>
+                <h1>Название домашки</h1>
 
                 <div>
-                    <h2>Уроки</h2>
+                    <h2>Приложения</h2>
 
-                    <Card title="123" href="/home"></Card>
+                    <Paper className="cards" elevation={6}
+                           sx={{bgcolor: '#cfe8fc', height: '100%', padding: 1}}>
+                        <Card title="123" href="/home"></Card>
+                        <Card title="123" href="/home"></Card>
+                        <Card title="123" href="/home"></Card>
+                        <Card title="123" href="/home"></Card>
+                    </Paper>
                 </div>
             </Container>
         </div>
