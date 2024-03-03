0d6ce4ccebafcc4d5aa5590d5a22d032c9f86a01
MrDrag0nXYT <147036304+MrDrag0nXYT@users.noreply.github.com>
2024-03-03 21:44:56 +0300

    theme changing

--- pstuedu/src/App.js
+++ pstuedu/src/App.js
@@ -1,18 +1,38 @@
-import './css/dark.css';
+import "./index.css";
 
-import NavigationBar from './components/NavigationBar'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { useState } from "react";
+
+import NavigationBar from "./components/NavigationBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
-import {Main} from "./components/Main";
+import { Main } from "./components/Main";
+
+const useTheme = () => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return { theme, toggleTheme };
+};
 
 function App() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="App">
-        <NavigationBar/>
-        <Routes>
-            <Route path={"/home"} element={<Main/>}></Route>
-            <Route path={"/main"} element={<Main/>}></Route>
-        </Routes>
+    <div className="App" className={theme}>
+      <NavigationBar />
+
+      <Routes>
+        <Route path={"/home"} element={<Main />}></Route>
+        <Route path={"/main"} element={<Main />}></Route>
+      </Routes>
+
+      <div>
+        <button onClick={toggleTheme}>Переключить тему</button>
+        <p>Текущая тема: {theme}</p>
+      </div>
     </div>
   );
 }
