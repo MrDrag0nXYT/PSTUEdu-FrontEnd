af2720db43461ad67486df32ed00d7ae1f66d002
zpub <lil.rhymes9@gmail.com>
2024-03-03 20:31:45 +0300

    React-Router-dom

--- pstuedu/src/App.js
+++ pstuedu/src/App.js
@@ -1,13 +1,18 @@
 import './css/dark.css';
 
 import NavigationBar from './components/NavigationBar'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import React from "react";
+import {Main} from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <NavigationBar />
-      </nav>
+        <NavigationBar/>
+        <Routes>
+            <Route path={"/home"} element={<Main/>}></Route>
+            <Route path={"/main"} element={<Main/>}></Route>
+        </Routes>
     </div>
   );
 }
---
+++ pstuedu/src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Main = () => {
+    return (
+        <div>
+            <div>Main</div>
+            <p>Helo</p>
+            <p>Helo</p>
+            <p>Helo</p>
+            <p>Helo</p>
+            <p>Helo</p>
+            <p>Helo</p>
+            <p>Helo</p>
+        </div>
+    )
+}
+
